cc5f5d05d1213099dda6f3ae614abbcba581e6d4
Makar <krikazon@mail.ru>
2023-01-24 21:29:33 +0700

    config system rework. logic separated from data. all configuration is now stored in json files.

--- Config/direct_config.py
+++ Config/direct_config.py
@@ -1,4 +1,39 @@
-TARGET_DIGITS = 16
+import json
+from os import path, chdir
+from time import sleep
+
+
+config_standard = {
+    "TARGET_DIGITS": 6
+}
+filename = "direct_config.json"
+
+
+CFG = path.dirname(__file__)
+chdir(CFG)
+try:
+    with open(filename, 'r') as config_file:
+        config: dict = json.load(config_file)
+    
+    if config.keys() != config_standard.keys():
+        raise KeyError
+    
+    
+except (FileNotFoundError, KeyError):
+    with open(filename, 'w') as config_file:
+        config_file.write(json.dumps(config_standard, indent=4))
+        
+    config = config_standard
+
+
+TARGET_DIGITS = config["TARGET_DIGITS"]
+
 
 # ####################################################################
-assert TARGET_DIGITS >= 1, "TARGET_DIGITS must be a positive integer"
+try:
+    assert TARGET_DIGITS >= 1, "const.TARGET_DIGITS must be a positive integer"
+except AssertionError as e:
+    print(f"Config validation failed: DIRECT > {e}")
+    if __name__ != "__main__":
+        input(">...")
+        exit(0)
--- Config/iterative_config.py
+++ Config/iterative_config.py
@@ -1,15 +1,56 @@
-MIN_DIGITS      = 2
-MAX_DIGITS      = 16
-PROCESSES       = 6
+import json
+from os import path, chdir
+from time import sleep
 
-UPPER_EXP_LIMIT = 10
-LOWER_EXP_LIMIT = 1
-LOOKUP_SIZE     = 256
+
+config_standard = {
+    "MIN_DIGITS"        : 2,
+    "MAX_DIGITS"        : 6,
+    "PROCESSES"         : 2,
+    "UPPER_EXP_LIMIT"   : 10,
+    "LOWER_EXP_LIMIT"   : 1,
+    "LOOKUP_SIZE"       : 256
+}
+filename = "iterative_config.json"
+
+
+CFG = path.dirname(__file__)
+chdir(CFG)
+try:
+    with open(filename, 'r') as config_file:
+        config: dict = json.load(config_file)
+    
+    if config.keys() != config_standard.keys():
+        raise KeyError
+    
+    
+except (FileNotFoundError, KeyError):
+    with open(filename, 'w') as config_file:
+        config_file.write(json.dumps(config_standard, indent=4))
+        
+    config = config_standard
+    
+    sleep(3)
+
+
+MIN_DIGITS      = config["MIN_DIGITS"]
+MAX_DIGITS      = config["MAX_DIGITS"]
+PROCESSES       = config["PROCESSES"]
+
+UPPER_EXP_LIMIT = config["UPPER_EXP_LIMIT"]
+LOWER_EXP_LIMIT = config["LOWER_EXP_LIMIT"]
+LOOKUP_SIZE     = config["LOOKUP_SIZE"]
 
 
 # ####################################################################################################################
-assert LOOKUP_SIZE > MAX_DIGITS + UPPER_EXP_LIMIT + 1,  "Increase const.LOOKUP_SIZE"
-assert PROCESSES >= 1,                                  "const.PROCESSES must be  at least 1"
-assert MIN_DIGITS >= 2,                                 "const.START_DIGITS must be greater than 2"
-assert MAX_DIGITS >= MIN_DIGITS,                        "const.MAX_DIGITS must be greater ot equal const.MIN_DIGITS"
-assert MIN_DIGITS - LOWER_EXP_LIMIT >= 0,               "Increase const.MIN_DIGITS or decrease const.LOWER_EXP_LIMIT"
+try:
+    assert LOOKUP_SIZE > MAX_DIGITS + UPPER_EXP_LIMIT + 1,  "Increase const.LOOKUP_SIZE"
+    assert PROCESSES >= 1,                                  "const.PROCESSES must be  at least 1"
+    assert MIN_DIGITS >= 2,                                 "const.MIN_DIGITS must be greater than 2"
+    assert MAX_DIGITS >= MIN_DIGITS,                        "const.MAX_DIGITS must be greater ot equal const.MIN_DIGITS"
+    assert MIN_DIGITS - LOWER_EXP_LIMIT >= 0,               "Increase const.MIN_DIGITS or decrease const.LOWER_EXP_LIMIT"
+except AssertionError as e:
+    print(f"Config validation failed: ITERATIVE > {e}")
+    if __name__ != "__main__":
+        input(">...")
+        exit(0)

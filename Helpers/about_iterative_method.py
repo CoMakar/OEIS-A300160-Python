f6ba3c03a4f542262da3f45118068fe7b53665f6
Makar <krikazon@mail.ru>
2023-02-10 05:05:42 +0700

    outdated info is now up to date

--- Helpers/about_iterative_method.py
+++ Helpers/about_iterative_method.py
@@ -3,14 +3,16 @@ from Common.Printer import Printer
            
 
 def help_me_to_understand(num_len=2, exponent=2):
-    exp_lookup = [i**exponent for i in range(9+1)]
-    cowr = list(ittls.combinations_with_replacement(exp_lookup, num_len))
-    cowrf = list(filter(lambda l: len(str(sum(l))) == num_len, cowr))
-    cowrfm = list(map(lambda l: (l-1, l+1), map(sum, cowrf)))
-    cowrfmc = list(ittls.chain.from_iterable(cowrfm))
-    cowrfmcset = set(cowrfmc)
+    cowr = list(ittls.combinations_with_replacement([d**exponent for d in range(9+1)], num_len))
+    cowrs = list(map(sum, cowr))
+    cowrf = list(filter(lambda l: len(str(l)) == num_len, cowrs))
+    cowrn = list(map(lambda l: (l-1, l+1), cowrf))
+    cowrc = list(ittls.chain.from_iterable(cowrn))
+    cowrcset = set(cowrc)
+    
     
     printer = Printer(4, 16)
+    
     
     print()
     print(f"(cowr)          (amount = {len(cowr)}; 100%)")
@@ -20,31 +22,37 @@ def help_me_to_understand(num_len=2, exponent=2):
         print("...")
     
     print()
+    print(f"(cowrs)         (amount = {len(cowrs)}; {len(cowrs) / len(cowr) * 100:.2f}% from base)")
+    if (num_len <= 3 or exponent <= 3):
+        printer.printf(cowrs)
+    else:
+        print("...")    
+        
+    print()
     print(f"(cowrf)         (amount = {len(cowrf)}; {len(cowrf) / len(cowr) * 100:.2f}% from base)")
     if (num_len <= 3 or exponent <= 3):
         printer.printf(cowrf)
     else:
         print("...")    
-        
     
     print()
-    print(f"(cowrfm)        (amount = {len(cowrfm)}; {len(cowrfm) / len(cowr) * 100:.2f}% from base)")
+    print(f"(cowrn)        (amount = {len(cowrn)}; {len(cowrn) / len(cowr) * 100:.2f}% from base)")
     if (num_len <= 3 or exponent <= 3):
-        printer.printf(cowrfm)
+        printer.printf(cowrn)
     else:
         print("...")
     
     print()
-    print(f"(cowrfmc)       (amount = {len(cowrfmc)}; {len(cowrfmc) / len(cowr) * 100:.2f}% from base)")
+    print(f"(cowrc)       (amount = {len(cowrc)}; {len(cowrc) / len(cowr) * 100:.2f}% from base)")
     if (num_len <= 3 or exponent <= 3):
-        printer.printf(cowrfmc)
+        printer.printf(cowrc)
     else:
         print("...")   
     
     print()
-    print(f"(cowrfmcset)    (amount = {len(cowrfmcset)}; {len(cowrfmcset) / len(cowr) * 100:.2f}% from base)")
+    print(f"(cowrcset)    (amount = {len(cowrcset)}; {len(cowrcset) / len(cowr) * 100:.2f}% from base)")
     if (num_len <= 3 or exponent <= 3):
-        printer.printf(cowrfmcset)
+        printer.printf(cowrcset)
     else:
         print("...")
         
@@ -52,5 +60,5 @@ def help_me_to_understand(num_len=2, exponent=2):
     print()
     print(f"There are {num_of_num_of_len_n} number of length {num_len}.")
     print(f"For the exponent = {exponent}:")
-    print(f"    We only need to process {len(cowrfmcset)} (only unique values) or {len(cowrfmc)} (with duplicates) of them.")
-    print(f"    It is {len(cowrfmcset) / num_of_num_of_len_n * 100:.2f}% and {len(cowrfmc) / num_of_num_of_len_n * 100:.2f}% of {num_of_num_of_len_n} respectively.")
+    print(f"    We only need to process {len(cowrcset)} (unique values) or {len(cowrc)} (with duplicates) of them.")
+    print(f"    These are {len(cowrcset) / num_of_num_of_len_n * 100:.8f}% and {len(cowrc) / num_of_num_of_len_n * 100:.8f}% of {num_of_num_of_len_n} respectively.")
